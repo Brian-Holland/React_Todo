317478ab3550c535aa1d32487724e6c3a3c691ae
Brian <brian.holland.pr@gmail.com>
2020-08-09 04:17:22 -0700

    added editing and removing

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export class NewTodoForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { task: '' };
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
+
+	handleChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		this.props.createTodo({ ...this.state, id: uuidv4() });
+		this.setState({
+			task: ''
+		});
+	}
+
 	render() {
 		return (
-			<form>
+			<form onSubmit={this.handleSubmit}>
 				<label htmlFor="task">New Todo</label>
-				<input type="text" placeholder="New Todo" id="task" />
+				<input
+					type="text"
+					placeholder="New Todo"
+					id="task"
+					name="task"
+					value={this.state.task}
+					onChange={this.handleChange}
+					required
+				/>
+				<button>Add</button>
 			</form>
 		);
 	}
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
+import NewTodoForm from './NewTodoForm';
 
 export class ToDoList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			todos: [ { task: 'Code' }, { task: 'Build Porject' } ]
+			todos: []
 		};
+		this.create = this.create.bind(this);
+		this.remove = this.remove.bind(this);
+	}
+
+	create(newTodo) {
+		this.setState({
+			todos: [ ...this.state.todos, newTodo ]
+		});
+	}
+	remove(id) {
+		this.setState({
+			todos: this.state.todos.filter((todo) => todo.id !== id)
+		});
 	}
 	render() {
 		const todos = this.state.todos.map((todo) => {
-			return <Todo task={todo.task} />;
+			return <Todo id={todo.id} key={todo.id} task={todo.task} removeTodo={this.remove} />;
 		});
 		return (
 			<div>
 				<h1>My ToDo List</h1>
+				<NewTodoForm createTodo={this.create} />
 				<ul>{todos}</ul>
 			</div>
 		);
